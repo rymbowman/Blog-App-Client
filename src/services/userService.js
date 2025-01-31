6bce33fcdadbd7b1f49fc5bbb1f5094060315da5
Ryan Bowman <rymbowman@gmail.com>
2025-01-31 11:39:28 -0500

    Updated route endpoints in the services files to connect with the backend server

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL + "/users";
+const API_URL = import.meta.env.VITE_API_URL + "/api/users";
 
 export const getUsers = async () => {
   try {
