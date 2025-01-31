cee10a76b261b47a60b4c19dbcc3cdbb4cc4d0c4
Ryan Bowman <rymbowman@gmail.com>
2025-01-31 18:42:28 -0500

    feat: Update App.jsx to integrate AuthProvider and ThemeProvider with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme/Theme";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  createRoutesFromElements,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Write from "./pages/Write";
 import NotFound from "./pages/NotFound";
@@ -23,33 +18,29 @@ import MainLayout from "./layouts/MainLayout";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
 
-const Router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<Home />} />
-      <Route path="/" element={<MainLayout />}>
-        <Route path="/write" element={<Write />} />
-        <Route path="/post/:id" element={<Post />} />
-        <Route path="/profile/:userId" element={<Profile />} />
-        <Route path="/edit/post/:id" element={<EditPost />} />
-        <Route path="/profile/:userId/edit" element={<EditProfile />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/terms-of-service" element={<TermsOfService />} />
-      </Route>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="reset-password/:token" element={<ResetPassword />} />
-      <Route path="*" element={<NotFound />} />
-    </>
-  )
-);
-
 function App() {
   return (
     <AuthProvider>
       <ThemeProvider theme={theme}>
-        <RouterProvider router={Router} />
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainLayout />}>
+              <Route index element={<Home />} />
+              <Route path="write" element={<Write />} />
+              <Route path="post/:id" element={<Post />} />
+              <Route path="profile/:userId" element={<Profile />} />
+              <Route path="edit/post/:id" element={<EditPost />} />
+              <Route path="profile/:userId/edit" element={<EditProfile />} />
+              <Route path="privacy-policy" element={<PrivacyPolicy />} />
+              <Route path="terms-of-service" element={<TermsOfService />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="forgot-password" element={<ForgotPassword />} />
+            <Route path="reset-password/:token" element={<ResetPassword />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Router>
       </ThemeProvider>
     </AuthProvider>
   );
