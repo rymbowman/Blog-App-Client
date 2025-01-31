64ee818000bd63a13fd7460bd7320001f01f0fe7
Ryan Bowman <rymbowman@gmail.com>
2025-01-31 17:37:37 -0500

    added logging to fetchPost function

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -58,6 +58,7 @@ const Post = () => {
   useEffect(() => {
     const fetchPost = async () => {
       const postData = await getPost(postId);
+      console.log(postData);
       setPost(postData);
     };
     fetchPost();
