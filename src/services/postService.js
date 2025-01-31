d14ab85852d16987dd65c3d7f718bdb0021c428a
Ryan Bowman <rymbowman@gmail.com>
2025-01-30 21:44:55 -0500

    Update API URLs to use environment variable

--- src/services/postService.js
+++ src/services/postService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/posts";
+const API_URL = import.meta.env.VITE_API_URL + "/posts";
+//"http://localhost:5000/api/posts";
 
 export const getPosts = async (userId) => {
   try {
