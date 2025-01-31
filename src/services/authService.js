24ced8a6421bbc5f26ef32e0ea84ccc02ad92811
Ryan Bowman <rymbowman@gmail.com>
2025-01-30 21:59:45 -0500

    Update API_URL in authService.js to use environment variable

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL + "/api/auth";
 //"http://localhost:5000/api/auth";
 
 export const registerUser = async (user) => {
