04158f2afd3499c7774286e865ad77bce6ecd357
Ryan Bowman <rymbowman@gmail.com>
2025-02-04 08:43:46 -0500

    style main container of NotFound page to center on screen

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,16 @@
-import { Box, Typography } from "@mui/material";
+import { Box, styled, Typography } from "@mui/material";
 import PrimaryButton from "../components/Features/PrimaryButton";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+
+const MainContainer = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+});
 
 const NotFound = () => {
   const navigate = useNavigate();
@@ -12,10 +20,10 @@ const NotFound = () => {
     navigate(`/profile/${user.id}`);
   };
   return (
-    <Box>
+    <MainContainer>
       <Typography variant="h1">404 Not Found</Typography>
       <PrimaryButton buttonText="Go home" action={handleClick} />
-    </Box>
+    </MainContainer>
   );
 };
 
